11deee25060947ca829713dc6c9b5ef3a85a814b
Calvin Dito Pratama <calvindito7@gmail.com>
2025-12-19 21:33:42 +0700

    fix: ternary operation number format

--- resources/views/physical-handover/delivery-accept-detail.blade.php
+++ resources/views/physical-handover/delivery-accept-detail.blade.php
@@ -48,11 +48,11 @@
                             <th class="table-success" width="20%">Resi</th>
                             <td width="30%">{{ $letter->RECEIPT_NO }}</td>
                             <th class="table-success" width="20%">Biaya Kirim</th>
-                            <td width="30%">Rp {{ number_format($letter->BIAYA_KIRIM) }}</td>
+                            <td width="30%">Rp {{ number_format($letter->BIAYA_KIRIM ?: 0) }}</td>
                         </tr>
                         <tr>
                             <th class="table-success" width="20%">Berat</th>
-                            <td width="30%">{{ number_format(($letter->BERAT ?? 0) / 1000, 2, ',', '.') }} Kg</td>
+                            <td width="30%">{{ number_format(($letter->BERAT ?: 0) / 1000, 2, ',', '.') }} Kg</td>
                             <th class="table-success" width="20%">Status</th>
                             <td width="30%">{{ $letter->STATUS }}</td>
                         </tr>
