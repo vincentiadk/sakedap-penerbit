3d72be966e4ef68fab21a8b9a7a3327b506a61f0
Calvin Dito Pratama <calvindito7@gmail.com>
2026-02-24 13:28:25 +0700

    fix: single upload isbn

--- config/barantum.php
+++ config/barantum.php
@@ -4,5 +4,4 @@ return [
     'base_url' => env('BARANTUM_BASE_URL'),
     'company_uuid' => env('BARANTUM_COMPANY_UUID'),
     'chat_bot_id' => env('BARANTUM_CHAT_BOT_ID'),
-    'template_id' => env('BARANTUM_TEMPLATE_ID'),
 ];
