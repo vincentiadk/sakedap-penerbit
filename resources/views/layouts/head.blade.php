dec33133f82c7c3885efae3cbe6abed3b610bbf3
Calvin Dito Pratama <calvindito7@gmail.com>
2026-03-16 12:35:12 +0700

    feat: google analytic

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -43,4 +43,12 @@
     @stack('readmore-js')
     @stack('lookup-js')
 	<script src="{{ asset('plugins/custom.js') }}?v={{ uniqid() }}"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-F4VZCGM4KE"></script>
+	<script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-F4VZCGM4KE');
+	</script>
 </head>
